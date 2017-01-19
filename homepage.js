f4c9092076fce5d76efcd2fa439313883a9d5a2a
陆玄青 <lu.xuanqing@outlook.com>
2017-01-19 18:30:17 +0800

    first version

---
+++ homepage.js
@@ -0,0 +1,16 @@
+'use strict'
+const express = require('express')
+const port = 8000
+
+var app = express()
+var server = app.listen(port, () => {
+  var host = server.address().address
+  var port = server.address().port
+  console.log(`Home page is running at http://${host}:${port}`)
+})
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('/index.html')
+})
